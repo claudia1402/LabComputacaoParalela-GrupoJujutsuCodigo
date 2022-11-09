31f09e85dff2dfa9326c7e667f56a5c0cc674215
João Pimenta <joaovictorfp30@gmail.com>
2022-11-08 23:39:03 -0300

    proj versoes teste

---
+++ ProjV3/projv310.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <omp.h>
+
+long double calculoFatorial(long double c) {
+  if (c == 0) {
+    return 1;
+  }
+  long double fatorial = 1;
+  for(int i = 1; i <= c; i++) {
+    fatorial *= i;
+  }
+  return fatorial;
+}
+
+int main() {
+  
+  const int N=10000; 
+
+  long double local_sum, sum; 
+
+//numero de Threads
+#pragma omp set_num_threads(10);
+#pragma omp parallel private(local_sum) shared(sum) 
+  { 
+    local_sum = 0; 
+    
+#pragma omp for
+    for (int i = 0; i <= N; i++) {
+      local_sum += 1/calculoFatorial(i); 
+    }
+
+
+#pragma omp critical 
+    sum += local_sum;
+  } 
+
+
+  printf("serie de taylor = %Lf\n", sum);
+}
