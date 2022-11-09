2edce20cb3826aa21d24552631d2c86dc91e2dbd
João Pimenta <joaovictorfp30@gmail.com>
2022-11-08 22:05:31 -0300

    Create ProjV3.c

---
+++ ProjV3/ProjV3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <omp.h>
+
+long double calculoFatorial(long double c) {
+  if (c == 0) {
+    return 1;
+  }
+  long double fatorial = 1;
+  for(int i = 1; i <= c; i++) {
+    fatorial *= i;
+  }
+  return fatorial;
+}
+
+int main() {
+
+  const int N=100000; 
+
+  long double local_sum, sum; 
+  
+#pragma omp parallel private(local_sum) shared(sum) 
+  { 
+    local_sum = 0; 
+    
+#pragma omp for
+    for (int i = 0; i <= N; i++) {
+      local_sum += 1/calculoFatorial(i); 
+    }
+
+
+#pragma omp critical 
+    sum += local_sum;
+  } 
+
+
+  printf("serie de taylor = %Lf\n", sum);
+}
